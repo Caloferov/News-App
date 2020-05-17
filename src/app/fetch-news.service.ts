1e7f8f0831564e05911a13676a747ae35749de6e
kirkalof <kiril.kaloferov@bulpros.com>
2020-05-17 13:50:09 +0300

    Fetching one batch of news + responsive layout

--- src/app/fetch-news.service.ts
+++ src/app/fetch-news.service.ts
@@ -4,30 +4,43 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class FetchNewsService {
-  corsProxyUrl = 'https://cors-proxy.htmldriven.com/?url=';
+  // corsProxyUrl = 'https://cors-proxy.htmldriven.com/?url=';
+  corsProxyUrl = 'https://cors-anywhere.herokuapp.com/'
   dirRssUrl = 'https://dir.bg/feeds/rss';
   faktiRssUrl = this.corsProxyUrl + 'https://fakti.bg/feed';
   chasa24RssUrl = this.corsProxyUrl + 'https://www.24chasa.bg/Rss';
 
-  allItemsArr = [];
+  articles = [];
 
   constructor(private http: HttpClient) {
     this.getAllData();
-    console.log(this.allItemsArr);
+    console.log(this.articles);
   }
 
   async getAllData() {
     await this.getRSSAndPushNodeItemsToArray(this.dirRssUrl);
-    await this.getRSSAndPushNodeItemsToArray(this.faktiRssUrl);
-    await this.getRSSAndPushNodeItemsToArray(this.chasa24RssUrl);
+    // await this.getRSSAndPushNodeItemsToArray(this.faktiRssUrl);
+    // await this.getRSSAndPushNodeItemsToArray(this.chasa24RssUrl);
 
   }
 
   getRSSAndPushNodeItemsToArray(url) {
-    this.http.get(url, { responseType: 'text' }).subscribe((resp) => {
+    this.http.get(url, { responseType: 'text' }).subscribe(async (resp) => {
       let nodeList = new window.DOMParser().parseFromString(resp, "text/xml").querySelectorAll('item');
+    
       for (var i = 0, ref = nodeList.length; i < ref; i++) {
-        this.allItemsArr.push(nodeList[i]);
+       let title = nodeList[i].querySelector('title').innerHTML;
+       let link = nodeList[i].querySelector('link').innerHTML;
+       let pubDate = nodeList[i].querySelector('pubDate').innerHTML;
+       let imageURL = nodeList[i].querySelector('enclosure').attributes[0].nodeValue;
+
+       let article = {
+         title: title,
+         link: link,
+         pubDate: pubDate,
+         imageURL: imageURL
+       }
+        this.articles.push(article);
       }
     });
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FetchNewsService } from '../fetch-news.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,27 @@ import { FetchNewsService } from '../fetch-news.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonInfiniteScroll, {
+    read: true,
+    static: true
+  }) infiniteScroll: IonInfiniteScroll;
 
-  constructor(public fetchNewsService: FetchNewsService) {}
+  constructor(public fetchNewsService: FetchNewsService) { }
+
+
+  loadData(event) {
+    console.log(event);
+    event.target.complete();
+
+    // App logic to determine if all data is loaded
+    // and disable the infinite scroll
+    if (this.fetchNewsService.articles.length > 100) {
+      event.target.disabled = true;
+    }
+
+  }
 
   ngOnInit() {
-    
+
   }
 }
